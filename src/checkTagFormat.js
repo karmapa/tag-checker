486cd9f3bf91fcba556facdb2a46c3ed376c7de2
chiehan <chieh.an.hsu@gmail.com>
2016-12-21 14:54:35 +0800

    check more tag formats

--- src/checkTagFormat.js
+++ src/checkTagFormat.js
@@ -1,6 +1,15 @@
+const repo = process.argv[2];
 const emptyTag = /<[\s\/]*>/g;
 const noEndArrow = /<[^>]*?(\n|$)/g;
 const noStartArrow = /(^|\n|>)[^<|\n]*>/g;
+const tagRegexs = [
+  ['division', new RegExp('<division n="(\\d+?)" t=".+?" i18n="' + repo + '-division-\\1"\\/>')],
+  ['vol', /<vol n="\d+?-\d+?" t=".+?"\/>/],
+  ['sutra', /<sutra id=".+?"\/>/],
+  ['bampo', /<bampo n=".+?"( \w+?=".+?")*?\/>/],
+  ['head', /<head n="\d+?" t=".+?"( \w+?=".+?")*?\/>/],
+  ['(pb|jp)', /<(pb|jp) id="\d+?-\d+?-\d+?[a-d]?"\/>/]
+];
 
 import reportErr from './reportErr.js';
 
@@ -19,8 +28,24 @@ export default function checkTagFormat(textObjs) {
     let emptyTags = text.match(emptyTag) || [];
     let noEndArrows = text.match(noEndArrow) || [];
     let noStartArrows = text.match(noStartArrow) || [];
-    saveErr(fileName, emptyTags.concat(noEndArrows).concat(noStartArrows));
+    let wrongTagContents = checkTagContent(text);
+    saveErr(fileName, emptyTags.concat(noEndArrows).concat(noStartArrows).concat(wrongTagContents));
   });
 
-  reportErr(errMessages);
+  reportErr('Worng Tag Format', errMessages);
 };
+
+function checkTagContent(text) {
+  let wrongTagContents = [];
+  tagRegexs.forEach((tagRegex) => {
+    let tagType = tagRegex[0];
+    let findingRegex = new RegExp('^.+?' + tagType + '.+?$', 'gm');
+    let correctRegex = tagRegex[1];
+    text.replace(findingRegex, (str) => {
+      if (! str.match(correctRegex)) {
+        wrongTagContents.push(tagType + ': ' + str);
+      }
+    });
+  });
+  return wrongTagContents;
+}
