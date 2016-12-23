444a0783e315eafa6fe63d327a30a64abd525f82
chiehan <chieh.an.hsu@gmail.com>
2016-12-23 17:24:19 +0800

    check vol order

--- src/checkVolPbOrder.js
+++ src/checkVolPbOrder.js
@@ -15,10 +15,18 @@ export default function checkVolPbOrder(textObjs) {
 
   textObjs.forEach((textObj) => {
     let {text, fileName, hasVol, textVolN} = initText(textObj, volPbRegex);
+    let volMessage = lastFileName + ' ' + lastTextVolN + ' ' + fileName + ' ' + textVolN;
+ 
     if (textVolN === lastTextVolN && hasVol) {
-      errMessages.push('Vol tag should not in ' + fileName + ', repeat vol tag or vol tag should be put in ' + lastFileName);
+      errMessages.push('Vol tag in ' + fileName + 'may repeat in or should be put in ' + lastFileName);
+    }
+    else if (lastTextVolN && textVolN !== lastTextVolN) {
+      if (wrongVolOrder(lastTextVolN, textVolN, lastFileName, fileName, volMessage)) {
+        errMessages.push('Wrong vol order: ' + volMessage);
+      };
     }
 
+    lastTextVolN = textVolN, lastFileName = fileName;
  //   let volPbs = text.match(volPbRegex);
 
   });
@@ -51,10 +59,32 @@ function initText(textObj, volPbRegex) {
   let textVolN = hasVol ? text.match(volNRegex)[1] : text.match(pbVolNRegex)[1];
   return {text: text, fileName: textObj.fileName, hasVol: hasVol, textVolN: textVolN};
 }
+
+function wrongVolOrder(lastTextVolN, textVolN, lastFileName, fileName, volMessage) {
+  let {major: lastMajor, minor: lastMinor} = splitVolN(lastTextVolN);
+  let {major: thisMajor, minor: thisMinor} = splitVolN(textVolN);
+
+  let majorEqual = thisMajor === lastMajor;
+  let majorNormal = lastMajor + 1 === thisMajor;
+  let majorJump = thisMajor - lastMajor > 1;
+  let minorJump = thisMinor - lastMinor > 1;
+
+  if (majorJump || majorEqual && minorJump || majorNormal && thisMinor > 1) {
+    console.log('Warning! Missing vol: ' + volMessage);
+  }
+
+  if (thisMajor < lastMajor || majorEqual && thisMinor < lastMajor) {
+    return true;
+  }
+}
+
+function splitVolN(volN) {
+  let splits = volN.split('-');
+  return {major: Number(splits[0]), minor: Number(splits[1])};
+}
 // vol tag 會在每個檔案前面
 // 一個檔案只有一個 vol
 
-// 換檔案 vol n 改變，要檢查 vol n 順序
 // 換檔案 vol n 改變，要檢查有沒有 vol
 
 // 每個檔案的 vol n 相同
