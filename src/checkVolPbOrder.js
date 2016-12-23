eb67fe6f4e45848e5f1697faec401b8323b66609
chiehan <chieh.an.hsu@gmail.com>
2016-12-23 11:25:41 +0800

    add vol pb regex

--- src/checkVolPbOrder.js
+++ src/checkVolPbOrder.js
@@ -1,5 +1,21 @@
+const pbRegex = /<pb id="\d+?-\d+?-\d+?[abcd]"/;
+const volPbRegex = /<vol n="\d+?-\d+?"|<pb id="\d+?-\d+?-\d+?[abcd]"/g;
+const volPbWoLetterSuffixRegex = /<vol n="\d+?-\d+?"|<pb id="\d+?-\d+?-\d+?"/g;
+
 import reportErr from './reportErr.js';
 
 export default function checkVolPbOrder(textObjs) {
+  let majorVol, minorVol, pbNum, pbLetter;
 
+  let pbHasSuffixLetter = textObjs[0].text.match(pbRegex);
+  let findingRegex = pbHasSuffixLetter ? volPbRegex : volPbWoLetterSuffixRegex;
+
+  textObjs.forEach((textObj) => {
+    let fileName = textObj.fileName;
+    let text = textObj.text;
+
+//    while(volPbRegex.exec(text)) {
+      //console.log(volPbRegex.exec(text));
+//    };
+  });
 }
