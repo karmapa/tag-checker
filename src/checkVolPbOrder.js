2cf1fa00d11147a5d3a4e7218ec4ae3794535ec9
chiehan <chieh.an.hsu@gmail.com>
2016-12-23 15:14:25 +0800

    check first vol n

--- src/checkVolPbOrder.js
+++ src/checkVolPbOrder.js
@@ -1,21 +1,41 @@
-const pbRegex = /<pb id="\d+?-\d+?-\d+?[abcd]"/;
-const volPbRegex = /<vol n="\d+?-\d+?"|<pb id="\d+?-\d+?-\d+?[abcd]"/g;
-const volPbWoLetterSuffixRegex = /<vol n="\d+?-\d+?"|<pb id="\d+?-\d+?-\d+?"/g;
+const volRegex = /<vol n="(\d+?)-(\d+?)"/;
+const volNRegex = /<vol n="(.+?)"/;
+const pbVolNRegex = /<pb id="(\d+?-\d+?)-/;
+const pbSLregex = /<pb id="(\d+?)-(\d+?)-(\d+?)([abcd])"/;
+const pbNoSLregex = /<pb id="\d+?-\d+?-\d+?"/;
+const volPbSLRegex = /<vol n="\d+?-\d+?"|<pb id="\d+?-\d+?-\d+?[abcd]"/g;
+const volPbNoSLRegex = /<vol n="\d+?-\d+?"|<pb id="\d+?-\d+?-\d+?"/g;
 
 import reportErr from './reportErr.js';
 
 export default function checkVolPbOrder(textObjs) {
-  let majorVol, minorVol, pbNum, pbLetter;
+  let {pbRegex, volPbRegex, firstVolN} = initSet(textObjs[0]);
 
-  let pbHasSuffixLetter = textObjs[0].text.match(pbRegex);
-  let findingRegex = pbHasSuffixLetter ? volPbRegex : volPbWoLetterSuffixRegex;
+  textObjs.forEach((textObj, index) => {
+    let text = textObj.text, fileName = textObj.fileName;
+    let volPbs = text.match(volPbRegex);
 
-  textObjs.forEach((textObj) => {
-    let fileName = textObj.fileName;
-    let text = textObj.text;
-
-//    while(volPbRegex.exec(text)) {
-      //console.log(volPbRegex.exec(text));
-//    };
   });
+}
+
+function initSet(textObj) {
+  let text = textObj.text, fileName = textObj.fileName;
+  let pbHasSuffixLetter = text.match(pbSLregex);
+  let pbRegex = pbHasSuffixLetter ? pbSLregex : pbNoSLregex;
+  let volPbRegex = pbHasSuffixLetter ? volPbSLRegex : volPbNoSLRegex;
+  let firstVolN = checkFirstVolN(text, fileName);
+  return {pbRegex: pbRegex, volPbRegex: volPbRegex, firstVolN: firstVolN};
+}
+
+function checkFirstVolN(text, fileName) {
+  let volInfo = text.match(volNRegex);
+  if (! volInfo) {
+    console.log('Warning! No vol in', fileName);
+    volInfo = text.match(pbVolNRegex);
+  }
+  let volN = volInfo[1];
+  if (volN !== '1-1') {
+    console.log('Warning! Vol not start from 1-1', fileName);
+  }
+  return volN;
 }
