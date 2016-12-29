1e0106adf80f0fc75178723e05c118142cfc3896
chiehan <chieh.an.hsu@gmail.com>
2016-12-29 11:19:39 +0800

    check first sutra id or bampo n

--- src/checkSutraBampoOrder.js
+++ src/checkSutraBampoOrder.js
@@ -1,6 +1,6 @@
 const sutraBampoPbRegex = /<(sutra|bampo).+?>|<pb.+?>(?=([\s\S](?!<pb))*?(?=<sutra|<bampo))/g;
 const sutraRegex = /<sutra id="([\da-zA-Z]*?[a-zA-Z])((\d+?)([a-zA-Z])?)"/;
-const bampoRegex = /<bampo n="([\da-zA-Z]+?)\.(\d+?)(\.(\d+?))?"/;
+const bampoRegex = /<bampo n="([\da-zA-Z]+?)\.((\d+?)(\.(\d+?))?)"/;
 const pbRegex = /<pb id="(.+?)"/;
 
 export default function checkSutraBampoOrder(textObjs) {
@@ -24,7 +24,33 @@ export default function checkSutraBampoOrder(textObjs) {
 }
 
 function checkOrder(lastBio, tagBio) {
-  console.log(tagBio);
+  if (lastBio) {
+
+  }
+  else {
+    checkFirstBio(tagBio);
+  }
+}
+
+function checkFirstBio(bio) {
+  checkFirstSutraNL(bio.sutraNL);
+
+  if (bio.type === 'bampo') {
+    console.log('Warning! No sutra before first bampo');
+    checkFirstBampoN(bio.bampoN);
+  }
+}
+
+function checkFirstSutraNL(sutraNL) {
+  if (sutraNL !== '1' && sutraNL !== '1a' && sutraNL !== '1A') {
+    console.log('Warning! Sutra id not start from 1, 1a, or 1A');
+  }
+}
+
+function checkFirstBampoN(bampoN) {
+  if (bampoN !== '1' && bampoN !== '1.1') {
+    console.log('Warning! Bampo n not start from 1 or 1.1');
+  }
 }
 
 function analyzeTag(tag, pb, fn) {
@@ -54,8 +80,9 @@ function analyzeBampo(tag, pb, fn) {
     pb: pb,
     fn: fn,
     sutraNL: bio[1],
-    bampo1n: bio[2],
-    bampo2n: bio[4]
+    bampoN: bio[2],
+    bampo1n: bio[3],
+    bampo2n: bio[5]
   }
 }
 
