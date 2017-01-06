d1d60cea3c2523347d4a308ad655c07a9deda52f
chiehan <chieh.an.hsu@gmail.com>
2017-01-06 14:17:18 +0800

    fix no start and end arrow tag regex

--- src/regexs.js
+++ src/regexs.js
@@ -11,12 +11,12 @@ export const bampoCIRegex = /<bampo n="((\d+?)([a-zA-Z])?)\.((\d+?)(\.(\d+?))?)"
 export const headSIRegex = /<head n="(\d+?)"/;
 export const pbSIRegex = /<pb id="(.+?)"/;
 
-export const volDRegex = /<vol/;
+export const volDRegex = /vol/;
 export const sutraDRegex = /sutra/;
 export const bampoDRegex = /bampo/;
-export const headDRegex = /<head/;
-export const pbDRegex = /<pb/;
+export const headDRegex = /head/;
+export const pbDRegex = /pb/;
 
 export const emptyTag = /<[\s\/]*>/g;
-export const noEndArrow = /<[^>]*?(\n|$)/g;
-export const noStartArrow = /(^|\n|>)[^<|\n]*>/g;
+export const noEndArrow = /<[^>]*?(<|\n|$)/g;
+export const noStartArrow = /(^|\n|>)[^<\n]*>/g;
