8120da710def5347a8d5c623ee4cc2ad87ad8a23
chiehan <chieh.an.hsu@gmail.com>
2017-01-11 11:17:03 +0800

    separate report err and errs

--- src/handleErr.js
+++ src/handleErr.js
@@ -6,11 +6,17 @@ export function reportErr(errType, errMessages) {
   }
 };
 
-export function saveErr(wholeErrs, message, newErrs = []) {
-  if (message) {
-    wholeErrs.splice(wholeErrs.length - 1, 0, message);
+export function saveErrs(wholeErrs, newErrs = [], additionalMessage) {
+  if (additionalMessage) {
+    wholeErrs.splice(wholeErrs.length - 1, 0, additionalMessage);
   }
   if (newErrs.length > 0) {
     wholeErrs.splice(wholeErrs.length - 1, 0, ...newErrs);
   }
 };
+
+export function saveErr(wholeErrs, errMessage) {
+  if (errMessage) {
+    wholeErrs.splice(wholeErrs.length - 1, 0, errMessage);
+  }
+} 
