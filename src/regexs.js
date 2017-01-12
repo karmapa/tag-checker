49bdd1032a67e8656d7c1ce433e68f5f597c2bf4
chiehan <chieh.an.hsu@gmail.com>
2017-01-12 18:30:58 +0800

    rm useless code

--- src/regexs.js
+++ src/regexs.js
@@ -1,14 +1,3 @@
-// S for Simple, X for compleX, I for getting Info, g for global
-// D for Detect, LD for Detect in Line W for Whole tag
-
-//export const divXWgRegex = new RegExp('<division n="(\\d+?)" t="[^<>\n]+?" i18n="' + repo + '-division-\\1"\\/>', 'g');
-//export const volXWgRegex = /<vol n="\d+?" t="[\u0f00-\u0fff]+?"\/>/g;
-//export const sutraXWgRegex = /<sutra id="[\da-zA-Z]*?[a-zA-Z]\d+?[a-zA-Z]?"\/>/g;
-//export const bampoXWgRegex = /<bampo n="\d+?[a-zA-Z]?\.\d+?(\.\d+?)?"\/>/g;
-//export const headXWgRegex = /<head n="\d+?" t="[\u0f00-\u0fff ]+?"( (type|zh|lv|st)="[^<>\n]+?")*?\/>/g;
-//export const jpb4XWgRegex = /<(pb|jp) id="\d+?-\d+?-\d+?[abcd]"\/>/g;
-//export const jpbXWgRegex = /<(pb|jp) id="\d+?-\d+?-\d+?"\/>/g;
-
 export const jpb4XWRegex = /<(pb|jp) id="\d+?-\d+?-\d+?[abcd]"\/>/;
 
 export const volHeadPbSWRegex = /<(vol|head).+?>|<pb.+?>(?=([\s\S](?!<pb))*?(?=<head))/g;
@@ -16,27 +5,11 @@ export const pbSWgRegex = /<pb id=.+?>/g;
 
 export const sutraXIRegex = /<sutra id="([\da-zA-Z]*?[a-zA-Z])((\d+?)([a-zA-Z])?)"/;
 export const bampoXIRegex = /<bampo n="((\d+?)([a-zA-Z])?)\.((\d+?)(\.(\d+?))?)"/;
-//export const pb4XIRegex = /<pb id="((\d+?)-(\d+?))-((\d+?)([abcd]?))"/;
-//export const pbXIRegex = /<pb id="((\d+?)-(\d+?))-(\d+?)"/;
 
-//export const divSIgRegex = /<division n="(\d+?)"/g;
-//export const pbSIgRegex = /<pb id="(.+?)"/g;
-//export const jpSIgRegex = /<jp id="(.+?)"/g;
-
-//export const volSIRegex = /<vol n="(\d+?)"/;
 export const headSIRegex = /<head n="(\d+?)"/;
 export const pbSIRegex = /<pb id="(.+?)"/;
 
-//export const divDgRegex = /div/g;
-//export const volDgRegex = /vol/g;
-//export const sutraDgRegex = /sutra/g;
-//export const bampoDgRegex = /bampo/g;
-//export const headDgRegex = /head/g;
-//export const jpbDgRegex = /pb|jp/g;
-
-//export const divDRegex = /div/;
 export const volDRegex = /vol/;
 export const sutraDRegex = /sutra/;
-//export const bampoDRegex = /bampo/;
 export const headDRegex = /head/;
 export const pbDRegex = /pb/;
