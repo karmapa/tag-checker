e17d0dd50d29cc9fd6a6fa80e23c83ae3a299833
chiehan <chieh.an.hsu@gmail.com>
2017-01-13 16:37:43 +0800

    add default regex to pbExist

--- src/detectTag.js
+++ src/detectTag.js
@@ -1,11 +1,12 @@
-const vol = /vol/;
+const vol = /<vol/;
+const pb = /<pb/;
 const pbWithSuffix = /<pb id="\d+?-\d+?-\d+?[abcd]"\/>/;
 
 export function detectPbType(str) {
   return pbWithSuffix.test(str);
 };
 
-export function pbExist(str, regex) {
+export function pbExist(str, regex = pb) {
   return regex.test(str);
 };
 
