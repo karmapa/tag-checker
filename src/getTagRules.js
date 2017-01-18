1df2b7df66e60a14b315a441adb1d53bf86d174a
chiehan <chieh.an.hsu@gmail.com>
2017-01-18 15:42:42 +0800

    update head regex

--- src/getTagRules.js
+++ src/getTagRules.js
@@ -38,7 +38,7 @@ let tagRules = [
   },
   {
     type: 'head',
-    correctRegex: /<head n="\d+?" t="[\u0f00-\u0fff ()]+?"( (type|zh|lv|st)="[^<>\n]+?")*?\/>/g,
+    correctRegex: /<head n="\d+?" t="[\u0f00-\u0fff ()]+?།"( (type|zh|lv|st)="[^<>\n]+?")*?\/>/g,
     suspectedRegex: /<head/g,
     tagNameStrRegex: /head/g,
     lineWithTagRegex: /^.*?head.*?$/mg
