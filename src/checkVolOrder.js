41ab3a6ee7ab5d84def4b6d2f708a26a1faaab57
chiehan <chieh.an.hsu@gmail.com>
2017-01-23 12:21:22 +0800

    rm init

--- src/checkVolOrder.js
+++ src/checkVolOrder.js
@@ -3,22 +3,35 @@ import {analyzeVol} from "./analyzeTag.js";
 import {warn, reportErr} from "./handleErr.js";
 
 export default function checkVolOrder(textObjs) {
-  let [firstText] = init(textObjs[0]);
-  let errMessages = [];
+  let lastVolBio, errMessages = [];
 
-  check1stTextVol(firstText);
+  check1stTextVol(textObjs[0].text);
+
+  textObjs.forEach((textObj) => {
+    let {fn, text} = textObj;
+
+    if (volExist(text)) {
+      let volBio = analyzeVol(text);
+
+      if (lastVolBio) {
+        check2VolOrder(errMessages, lastVolBio, volBio);
+      }
+      else {
+        lastVolBio = volBio;
+      }
+    }
+  });
 
   reportErr('Error! Wrong Tag Order!', errMessages);
 };
-
-function init(textObj) {
-  let {fn, text} = textObj;
-  return [text];
-}
 
 function check1stTextVol(text) {
   let volN = analyzeVol('first file', text).volN;
   if (! volExist(text) || volN !== '1' && volN !== '1-1') {
     warn('No vol tag or volN not 1 or 1-1 in first file!');
   }
+}
+
+function check2VolOrder(store, lastBio, bio) {
+
 }
