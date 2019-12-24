57d01afc0c5e75203dfc7a3218563b97016b2cd4
chiehan1 <chieh.an.hsu@gmail.com>
2019-12-24 11:26:33 +0800

    fix regex

--- src/checkTagFormat.js
+++ src/checkTagFormat.js
@@ -6,7 +6,7 @@ import {sameNumber} from './compareNumber.js';
 const emptyTagRegex = /<[\s\/]*>/g;
 const noEndArrowRegex = /<[^>]*?(<|\n|$)/g;
 const noStartArrowRegex = /(^|\n|>)[^<\n]*?>/g;
-const undefinedTagRegex = /<(?!(division|vol|head|pb|jp|sutra|bampo|headnote|\/?stitle|\/?ttitle))
+const undefinedTagRegex = /<(?!(division|vol|head|pb|jp|sutra|bampo|headnote|\/?stitle|\/?ttitle)).+?/g;
 
 export default function checkTagFormat(textObjs, pbWithSuffix, looseMode) {
   let errMessages = [];
